825cf79ac3cd99fe8a80bf0786db89f9f75de1a9
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-07 14:30:35 -0600

    Create my_scanf.c

---
+++ my_scanf.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <stdio.h>
+
+#define MAX_BUF_SIZE 1024
+
+int my_scanf(const char *format, ...) {
+    char buf[MAX_BUF_SIZE];
+    int read_bytes = read(0, buf, MAX_BUF_SIZE - 1);
+    if (read_bytes <= 0) return -1;
+    buf[read_bytes] = '\0';
+
+    va_list args;
+    va_start(args, format);
+    char *str;
+    int *int_ptr;
+    float *float_ptr;
+    int base;
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'd': {
+                    int_ptr = va_arg(args, int*);
+                    sscanf(buf, "%d", int_ptr);
+                    break;
+                }
+                case 's': {
+                    str = va_arg(args, char*);
+                    sscanf(buf, "%s", str);
+                    break;
+                }
+                case 'x': {
+                    int_ptr = va_arg(args, int*);
+                    sscanf(buf, "%x", int_ptr);
+                    break;
+                }
+                case 'b': {
+                    int_ptr = va_arg(args, int*);
+                    sscanf(buf, "%b", int_ptr);
+                    break;
+                }
+                default:
+                    return -1;
+            }
+        }
+        format++;
+    }
+    va_end(args);
+    return 0;
+}
