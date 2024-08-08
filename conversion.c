9b58260be812aebf8fa9b959873e572668d5095f
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-08 09:29:22 -0600

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 
-void int_to_str(int num, char *buffer, int base) {
-    if (base == 2) {
-        // Convierto a binario manualmente
-        for (int i = 31; i >= 0; i--) {
-            buffer[31 - i] = (num & (1 << i)) ? '1' : '0';
-        }
-        buffer[32] = '\0';
-    } else if (base == 16) {
-        // Convierto a hexadecimal, sólo los 4 dígitos necesarios
-        snprintf(buffer, 5, "%04X", num & 0xFFFF);
-    } else {
-        snprintf(buffer, 12, "%d", num);
-    }
+int str_to_int(const char *str, int base) {
+    return (int)strtol(str, NULL, base);
 }
 
-int str_to_int(char *str, int base) {
-    return (int)strtol(str, NULL, base);
+void int_to_str(int num, char *str, int base) {
+    char buffer[32];
+    int i = 0, sign = 0;
+    if (num < 0 && base == 10) {
+        sign = 1;
+        num = -num;
+    }
+    do {
+        int digit = num % base;
+        buffer[i++] = (digit < 10) ? (digit + '0') : (digit - 10 + 'a');
+        num /= base;
+    } while (num);
+    if (sign) buffer[i++] = '-';
+    buffer[i] = '\0';
+    // Reverse buffer
+    for (int j = 0; j < i; j++) {
+        str[j] = buffer[i - j - 1];
+    }
+    str[i] = '\0';
 }
