b75291ae6b8734385e42d707dfb813d8adf4b1b9
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-08 09:34:51 -0600

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
 int str_to_int(const char *str, int base) {
-    return (int)strtol(str, NULL, base);
+    return strtol(str, NULL, base);
 }
 
 void int_to_str(int num, char *str, int base) {
-    char buffer[32];
-    int i = 0, sign = 0;
-    if (num < 0 && base == 10) {
-        sign = 1;
-        num = -num;
+    if (base == 2) {
+        char buffer[33];
+        itoa(num, buffer, base);
+        strcpy(str, buffer);
+    } else if (base == 16) {
+        sprintf(str, "%x", num);
+    } else {
+        sprintf(str, "%d", num);
     }
-    do {
-        int digit = num % base;
-        buffer[i++] = (digit < 10) ? (digit + '0') : (digit - 10 + 'a');
-        num /= base;
-    } while (num);
-    if (sign) buffer[i++] = '-';
-    buffer[i] = '\0';
-    // Reverse buffer
-    for (int j = 0; j < i; j++) {
-        str[j] = buffer[i - j - 1];
-    }
-    str[i] = '\0';
 }
