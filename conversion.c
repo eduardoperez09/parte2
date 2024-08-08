c7cdde3e1c38534bf2045dd0b4c715724c8973a8
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-07 20:24:51 -0600

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 
-void int_to_str(int num, char *buffer, int base) {
-    if (base == 2) {
-        // Convierto a binario manualmente
-        for (int i = 31; i >= 0; i--) {
-            buffer[31 - i] = (num & (1 << i)) ? '1' : '0';
-        }
-        buffer[32] = '\0';
-    } else if (base == 16) {
-        // Convierto a hexadecimal, sólo los 4 dígitos necesarios
-        snprintf(buffer, 5, "%04X", num & 0xFFFF);
-    } else {
-        snprintf(buffer, 12, "%d", num);
+int str_to_int(const char *str, int base) {
+    return strtol(str, NULL, base);
+}
+
+void int_to_str(int num, char *str, int base) {
+    if (base == 16) {
+        snprintf(str, 5, "%04X", num & 0xFFFF);
+    } else if (base == 10) {
+        snprintf(str, 12, "%d", num);
+    } else if (base == 2) {
+        // Handle binary conversion here if needed
     }
 }
 
-int str_to_int(char *str, int base) {
-    return (int)strtol(str, NULL, base);
-}
