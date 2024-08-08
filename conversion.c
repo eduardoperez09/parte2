a83d0c37fe8c37924df3f0382267d515776ca07e
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-07 20:13:35 -0600

    Update conversion.c

--- conversion.c
+++ conversion.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <math.h>
 
 void int_to_str(int num, char *buffer, int base) {
     if (base == 2) {
+        // Convierto a binario manualmente
         for (int i = 31; i >= 0; i--) {
             buffer[31 - i] = (num & (1 << i)) ? '1' : '0';
         }
         buffer[32] = '\0';
     } else if (base == 16) {
+        // Convierto a hexadecimal, sólo los 4 dígitos necesarios
         snprintf(buffer, 5, "%04X", num & 0xFFFF);
     } else {
         snprintf(buffer, 12, "%d", num);
