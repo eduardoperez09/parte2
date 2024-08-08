738e1bba4cbe6fe6cf3a61ec8c1529220dd75364
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-07 20:46:04 -0600

    Update my_io.h

--- my_io.h
+++ my_io.h
@@ -4,7 +4,4 @@
 void my_printf(const char *format, ...);
 void my_scanf(const char *format, ...);
 
-int str_to_int(const char *str, int base);
-void int_to_str(int num, char *str, int base);
-
-#endif // MY_IO_H
+#endif
