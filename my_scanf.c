fa548ea8f8354d97601018fdef405cef845da735
eduardoperez09 <161544625+eduardoperez09@users.noreply.github.com>
2024-08-08 09:56:01 -0600

    Update my_scanf.c

--- my_scanf.c
+++ my_scanf.c
@@ -1,50 +1,58 @@
 #include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "my_io.h"
 
-#define MAX_BUF_SIZE 1024
+int str_to_int(const char *str, int base);
 
-int my_scanf(const char *format, ...) {
-    char buf[MAX_BUF_SIZE];
-    int read_bytes = read(0, buf, MAX_BUF_SIZE - 1);
-    if (read_bytes <= 0) return -1;
-    buf[read_bytes] = '\0';
-
+void my_scanf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    char *str;
+    char buffer[32];
     int *int_ptr;
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-                case 'd': {
-                    int_ptr = va_arg(args, int*);
-                    sscanf(buf, "%d", int_ptr);
+    char *str;
+    int num_read;
+
+    for (int i = 0; format[i] != '\0'; i++) {
+        if (format[i] == '%') {
+            i++;
+            switch (format[i]) {
+                case 'd':
+                    num_read = read(0, buffer, 32);
+                    buffer[num_read - 1] = '\0';  // Eliminar el '\n'
+                    int_ptr = va_arg(args, int *);
+                    *int_ptr = str_to_int(buffer, 10);
                     break;
-                }
-                case 's': {
-                    str = va_arg(args, char*);
-                    sscanf(buf, "%s", str);
+                case 's':
+                    str = va_arg(args, char *);
+                    num_read = read(0, str, 32);
+                    str[num_read - 1] = '\0';  // Eliminar el '\n'
                     break;
-                }
-                case 'x': {
-                    int_ptr = va_arg(args, int*);
-                    sscanf(buf, "%x", int_ptr);
+                case 'x':
+                    num_read = read(0, buffer, 32);
+                    buffer[num_read - 1] = '\0';  // Eliminar el '\n'
+                    int_ptr = va_arg(args, int *);
+                    *int_ptr = str_to_int(buffer, 16);
                     break;
-                }
-                case 'b': {
-                    int_ptr = va_arg(args, int*);
-                    sscanf(buf, "%d", int_ptr); // %b no es estándar, usa %d y maneja la conversión
+                case 'b':
+                    num_read = read(0, buffer, 32);
+                    buffer[num_read - 1] = '\0';  // Eliminar el '\n'
+                    int_ptr = va_arg(args, int *);
+                    *int_ptr = str_to_int(buffer, 2);
                     break;
-                }
                 default:
-                    return -1;
+                    break;
             }
         }
-        format++;
     }
+
     va_end(args);
-    return 0;
+}
+
+int str_to_int(const char *str, int base) {
+    int result = 0, i = 0;
+    while (str[i] != '\0') {
+        result = result * base + (str[i] - '0');
+        i++;
+    }
+    return result;
 }
